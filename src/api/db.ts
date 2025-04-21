3aa735807a0715a533df130c2e5c942f58d4a38e
songhyun <deadline001@naver.com>
2025-04-21 09:27:05 +0900

    db 위치 재조정

--- src/api/db.ts
+++ src/api/db.ts
@@ -10,7 +10,7 @@ async function getDbPath() {
     const currentPlatform = platform();
     const home = await homeDir();
     if (currentPlatform === 'windows') {
-        return `sqlite:\\\\${await join(home, 'test.db')}`;
+        return "sqlite:test.db";
     } else {
         // Linux 또는 macOS        
         return `sqlite://${await join(home, 'test.db')}`;
